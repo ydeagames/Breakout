b94bb10fc3dfaa3547ffb3770b4e639b8972f37f
YdeaGames <ydeagames@gmail.com>
2018-10-25 18:50:43 +0900

    DrawUtils

--- Libraries/GameLibrary/DrawUtils.h
+++ Libraries/GameLibrary/DrawUtils.h
@@ -31,15 +31,17 @@ enum ColorCode : unsigned int
 };
 
 // <図形描画関連>
+namespace DrawUtils
+{
+	// 破線を描画
+	int DrawDashedLine(int x1, int y1, int x2, int y2, unsigned int color, int length, int thickness = 1);
 
-// 破線を描画
-int DrawDashedLine(int x1, int y1, int x2, int y2, unsigned int color, int length, int thickness = 1);
+	// 破線を描画(アンチエイリアス効果付き)
+	int DrawDashedLineAA(float x1, float y1, float x2, float y2, unsigned int color, float length, float thickness = 1.0f);
 
-// 破線を描画(アンチエイリアス効果付き)
-int DrawDashedLineAA(float x1, float y1, float x2, float y2, unsigned int color, float length, float thickness = 1.0f);
+	// カプセルを描画
+	int DrawCapsule(int x1, int y1, int x2, int y2, int r, unsigned int color, int thickness = 1);
 
-// カプセルを描画
-int DrawCapsule(int x1, int y1, int x2, int y2, int r, unsigned int color, int thickness = 1);
-
-// カプセルを描画(アンチエイリアス効果付き)
-int DrawCapsuleAA(float x1, float y1, float x2, float y2, float r, unsigned int color, float thickness = 1.0f);
+	// カプセルを描画(アンチエイリアス効果付き)
+	int DrawCapsuleAA(float x1, float y1, float x2, float y2, float r, unsigned int color, float thickness = 1.0f);
+}
